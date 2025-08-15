f8cfd81d347eda101cd72f113aacc26dd6aa534b
priyankatubakad <priyankatubakad99@gmail.com>
2025-08-15 21:48:26 +0530

    Added Training scripts

--- Training/pytest_package/basic_vivek.py
+++ Training/pytest_package/basic_vivek.py
@@ -6,7 +6,9 @@ chrome_driver =webdriver.ChromeOptions()
 
 driver= webdriver.Chrome()
 
-driver.get('https://demowebshop.tricentis.com/')
+driver.get('https://48.ie/log-in')
 
 driver.find_element('css selector','button[class="btn btn-primary-outline"]').click()
+time.sleep(2)
+driver.find_element('css selector','input[id="fname"]').send_keys("vivekanandab@gmail.com")
 time.sleep(2)
